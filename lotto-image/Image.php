57f65d776450fad1d259f62db3138b2589f639bd
Henryk Helsky <henryk@restate.fi>
2013-11-20 01:52:11 +0200

    comments and tweaking

--- lotto-image/Image.php
+++ lotto-image/Image.php
@@ -8,13 +8,16 @@
 
 namespace Lotto;
 
-
 class Image {
 
     private $image;
     private $base_image = 'base.png';
     private $numbers     = [];
 
+    /**
+     * @param array $numbers
+     * @param bool|string $base_image
+     */
     public function __construct( array $numbers, $base_image = false )
     {
         if ($base_image) {
@@ -23,25 +26,24 @@ class Image {
 
         $this->numbers = $numbers;
         $this->image   = new \Imagick($this->base_image);
-        $this->createImageFromNumbers($numbers);
+        $this->createImageFromNumbers($this->numbers);
     }
 
+    /**
+     * @param $numbers array
+     */
     private function createImageFromNumbers($numbers)
     {
-        $draw = new \ImagickDraw();
-        $draw->setfont('Bookman-DemiItalic');
-        $draw->setfontsize(28);
-        $draw->settextalignment(2); // 1 left, 2 center, 3 right
-        $draw->setfillcolor('black');
+        $draw = $this->getPencil();
 
         $grid = [
-            ['x' => 52, 'y' => 58],
-            ['x' => 120, 'y' => 58],
-            ['x' => 185, 'y' => 58],
-            ['x' => 252, 'y' => 58],
-            ['x' => 317, 'y' => 58],
-            ['x' => 382, 'y' => 58],
-            ['x' => 450, 'y' => 58],
+            ['x' => 51, 'y' => 58],
+            ['x' => 117, 'y' => 58],
+            ['x' => 183, 'y' => 58],
+            ['x' => 250, 'y' => 58],
+            ['x' => 315, 'y' => 58],
+            ['x' => 381, 'y' => 58],
+            ['x' => 448, 'y' => 58],
         ];
 
         foreach($numbers as $key => $number) {
@@ -50,15 +52,33 @@ class Image {
                 $grid[$key]['x'],
                 $grid[$key]['y'],
                 0,
-                $number
+                (int) $number
             );
         }
     }
 
+    /**
+     * Send headers and print the image
+     */
     public function render()
     {
         header('Content-type: image/png;');
         echo $this->image;
+    }
+
+    /**
+     * @return \ImagickDraw
+     */
+    private function getPencil()
+    {
+        $draw = new \ImagickDraw();
+        $draw->setfont('Bookman-DemiItalic');
+        $draw->setfontsize(28);
+        $draw->settextalignment(2); // 1 left, 2 center, 3 right
+        $draw->setfillcolor('black');
+        $draw->setfillopacity(0.7);
+
+        return $draw;
     }
 
 }
@@ -68,21 +88,20 @@ if ($_GET && isset($_GET['numbers'])) {
 
     $numbers = explode(',', strip_tags($_GET['numbers']));
 
-    if (count($numbers) === 7) {
+    $numbers = array_map(function($val) {
+        return (int) $val;
+    }, $numbers);
 
+    if (count($numbers) === 7) {
         try {
-            $image = new \Lotto\Image($numbers);
-            $image->render();
+            (new \Lotto\Image($numbers))->render();
         }
-        catch(\Exception $e) {
+        catch(\ImagickException $e) {
             echo $e->getTraceAsString();
         }
-
-
-
     } else {
         die('Needs seven numbers');
     }
 } else {
-    die('xoo');
+    die(); // Silence
 }
