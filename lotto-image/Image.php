3955cdc28214e5d69daa599a7de79efea0c81281
Henryk Helsky <henryk@restate.fi>
2013-11-20 02:04:14 +0200

    fixore

--- lotto-image/Image.php
+++ lotto-image/Image.php
@@ -10,20 +10,24 @@ namespace Lotto;
 
 class Image {
 
+    /**
+     * @var \Imagick
+     */
     private $image;
-    private $base_image = 'base.png';
+    /**
+     * @var string
+     */
+    private $base_image  = 'base.png';
+    /**
+     * @var array
+     */
     private $numbers     = [];
 
     /**
      * @param array $numbers
-     * @param bool|string $base_image
      */
-    public function __construct( array $numbers, $base_image = false )
+    public function __construct( array $numbers )
     {
-        if ($base_image) {
-            $this->base_image = $base_image;
-        }
-
         $this->numbers = $numbers;
         $this->image   = new \Imagick($this->base_image);
         $this->createImageFromNumbers($this->numbers);
@@ -35,16 +39,7 @@ class Image {
     private function createImageFromNumbers($numbers)
     {
         $draw = $this->getPencil();
-
-        $grid = [
-            ['x' => 51, 'y' => 58],
-            ['x' => 117, 'y' => 58],
-            ['x' => 183, 'y' => 58],
-            ['x' => 250, 'y' => 58],
-            ['x' => 315, 'y' => 58],
-            ['x' => 381, 'y' => 58],
-            ['x' => 448, 'y' => 58],
-        ];
+        $grid = $this->getGrid();
 
         foreach($numbers as $key => $number) {
             $this->image->annotateimage(
@@ -81,13 +76,25 @@ class Image {
         return $draw;
     }
 
-}
+    private function getGrid()
+    {
+        return [
+            ['x' => 51, 'y' => 58],
+            ['x' => 117, 'y' => 58],
+            ['x' => 183, 'y' => 58],
+            ['x' => 250, 'y' => 58],
+            ['x' => 315, 'y' => 58],
+            ['x' => 381, 'y' => 58],
+            ['x' => 448, 'y' => 58],
+        ];
 
+    }
+
+}
 
 if ($_GET && isset($_GET['numbers'])) {
 
     $numbers = explode(',', strip_tags($_GET['numbers']));
-
     $numbers = array_map(function($val) {
         return (int) $val;
     }, $numbers);
